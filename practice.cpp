0c7ea760311d5b668db03da28108a4934f590bcc
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-04-20 00:25:23 +0530

    loops

--- practice.cpp
+++ practice.cpp
@@ -5,5 +5,18 @@ using namespace std;
 int main (){
     int age=  20;
     cout << age<< endl;
+
+    //Explicit Conversion 
+    double price = 100.99;
+    int newPrice =(int)price;
+    cout << newPrice<< endl;
+
+    //input variable
+    int age ;
+    cout<< "Enter the age : ";
+     cin>> age;
+    cout <<"Your age is :"<< age<<endl;
+
+    
     return 0;
 }

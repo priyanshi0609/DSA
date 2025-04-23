4bb0c95f61d11b18dfff05d2a70121a69c1222e8
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-04-23 15:40:18 +0530

    arrays

--- arrays.cpp
+++ arrays.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main (){
     int marks[5]={1,2,3,4,5};
+    int position ;
     int size=5;
     int smallest = INT_MAX;
-    int position ;
-
+    
 
     for (int i=0;i<size;i++){
         if (marks[i]<smallest){
@@ -17,6 +17,11 @@ int main (){
     }
     cout << "smallest number is : " << smallest << endl;
     cout << "position of smallest no:"<< position << endl;
+
+
+
+    // linear search algo 
+    int linearsearch
 
     return 0;
 }
